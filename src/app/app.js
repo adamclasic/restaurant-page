b5c05aff5adab3e7e68a1b50d16c98a69ee055df
adam allalou <aabou9@gmail.com>
2020-08-24 21:09:15 +0100

    fix active tab

--- src/app/app.js
+++ src/app/app.js
@@ -3,16 +3,11 @@ import { homePage } from "./utils/home.js";
 import { explorePage } from "./utils/explore.js";
 import { aboutPage } from "./utils/about.js";
 
-
-
 export const run = () => {
     let tabSwitching = (pageName) => {
         document.querySelectorAll('.nav-link').forEach((elem) => {elem.style.backgroundColor = 'red'});
         document.getElementById(pageName).style.backgroundColor = 'yellow';
-        
-        // homePage = homePage();
-        // explorePage = explorePage();
-        // aboutPage = aboutPage();
+
         let printContent = '';
         switch (pageName) {
             case 'home':
@@ -31,24 +26,23 @@ export const run = () => {
         main.innerHTML = '';
         main.appendChild(printContent);
     };
-    
+
     let navLinksArray = ['home', 'explore', 'about'];
     let workPlace = document.getElementById('content');;
     makeHeader(navLinksArray, workPlace);
-    
     let main = document.createElement('main');
+
+    tabSwitching('home');
     document.getElementById("home").addEventListener("click", function(){ 
         tabSwitching('home');
     });
-    
+
     document.getElementById("explore").addEventListener("click", function(){ 
         tabSwitching('explore');
     });
-    
+
     document.getElementById("about").addEventListener("click", function(){ 
         tabSwitching('about');
     });
     workPlace.appendChild(main);
-
-        
 }
