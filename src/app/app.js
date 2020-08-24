9b88e1e63cfa0b1bc11a1d58fb9b2c8492ac6b9a
adam allalou <aabou9@gmail.com>
2020-08-24 18:25:33 +0100

    add switch logic to tabs

--- src/app/app.js
+++ src/app/app.js
@@ -9,27 +9,46 @@ export const run = () => {
     let tabSwitching = (pageName) => {
         document.querySelectorAll('.nav-link').forEach((elem) => {elem.style.backgroundColor = 'red'});
         document.getElementById(pageName).style.backgroundColor = 'yellow';
+        
+        // homePage = homePage();
+        // explorePage = explorePage();
+        // aboutPage = aboutPage();
+        let printContent = '';
+        switch (pageName) {
+            case 'home':
+                printContent = document.createElement('div').innerText = homePage();
+                break;
+            case 'explore':
+                printContent = document.createElement('div').innerText = explorePage();
+                break;
+            case 'about':
+                printContent = document.createElement('div').innerText = aboutPage();
+                break;
+            default:
+                printContent = 'something went wrong.'
+            }
+
+        main.innerHTML = '';
+        main.appendChild(printContent);
     };
     
     let navLinksArray = ['home', 'explore', 'about'];
     let workPlace = document.getElementById('content');;
-    let explorePageCont = explorePage();
     makeHeader(navLinksArray, workPlace);
-    // workPlace.appendChild(explorePageCont)
-    // add mainf
+    
+    let main = document.createElement('main');
     document.getElementById("home").addEventListener("click", function(){ 
         tabSwitching('home');
     });
-
+    
     document.getElementById("explore").addEventListener("click", function(){ 
         tabSwitching('explore');
     });
-
+    
     document.getElementById("about").addEventListener("click", function(){ 
         tabSwitching('about');
     });
+    workPlace.appendChild(main);
 
         
-    let main = document.createElement('main');
-    workPlace.appendChild(main);
 }
