022a0f486cf5e103224e67232bb138a48ff4390c
adam allalou <aabou9@gmail.com>
2020-08-25 13:48:11 +0100

    add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|ttf)$/i,
         use: [
           {
             loader: 'file-loader',
