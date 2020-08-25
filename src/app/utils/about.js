93f57f556e51aa4fe7a3f006d9c3c162f7db30d4
adam allalou <aabou9@gmail.com>
2020-08-25 14:55:51 +0100

    add html to about, explore

--- src/app/utils/about.js
+++ src/app/utils/about.js
@@ -1,8 +1,8 @@
 export const aboutPage = () => {
     
     let aboutContent = document.createElement('div');
-    let loremText = `ABOUT PAGE Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis similique, doloremque, neque ab libero sit debitis, aspernatur ex quam consequuntur iure architecto molestias ducimus inventore voluptas? Similique sequi nulla facere repellendus, error ratione odio corporis animi veniam hic commodi quibusdam eveniet ad quis, voluptas tempore, et officiis! Aspernatur, vitae!`;
-    aboutContent.innerText = loremText;
+    let loremText = `<h1> ABOUT PAGE</h1> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis similique, doloremque, neque ab libero sit debitis, aspernatur ex quam consequuntur iure architecto molestias ducimus inventore voluptas? Similique sequi nulla facere repellendus, error ratione odio corporis animi veniam hic commodi quibusdam eveniet ad quis, voluptas tempore, et officiis! Aspernatur, vitae!</p>`;
+    aboutContent.innerHTML = loremText;
     aboutContent.id = 'aboutContDiv';
     return aboutContent;
 }
--- src/app/utils/explore.js
+++ src/app/utils/explore.js
@@ -1,8 +1,8 @@
 export const explorePage = () => {
     
     let exploreContent = document.createElement('div');
-    let loremText = `EXPLOR PAGE Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis similique, doloremque, neque ab libero sit debitis, aspernatur ex quam consequuntur iure architecto molestias ducimus inventore voluptas? Similique sequi nulla facere repellendus, error ratione odio corporis animi veniam hic commodi quibusdam eveniet ad quis, voluptas tempore, et officiis! Aspernatur, vitae!`;
-    exploreContent.innerText = loremText;
+    let loremText = `<h1> EXPLOR PAGE</h1> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis similique, doloremque, neque ab libero sit debitis, aspernatur ex quam consequuntur iure architecto molestias ducimus inventore voluptas? Similique sequi nulla facere repellendus, error ratione odio corporis animi veniam hic commodi quibusdam eveniet ad quis, voluptas tempore, et officiis! Aspernatur, vitae!</p>`;
+    exploreContent.innerHTML = loremText;
     exploreContent.id = 'exploreContDiv';
     return exploreContent;
 }
